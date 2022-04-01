d557a8f7cc94856a527ef6cf3ceb7e2284f062ec
Scott <johnson.scott.14@gmail.com>
2022-04-01 16:36:49 -0600

    Added initial detection, spheres, and colors to objects:

--- Assets/ConeCollider/Samples/Sample_1/InCheck.cs
+++ Assets/ConeCollider/Samples/Sample_1/InCheck.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class InCheck : MonoBehaviour {
 
     private Text text;
+    public List<GameObject> ConeCollisions;
 
     void Awake()
     {
@@ -13,24 +15,27 @@ public class InCheck : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        ConeCollisions = new List<GameObject>();
+        text.color = Color.red;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        text.text = "";
+        for(int x = 0; x < ConeCollisions.Count; x ++){
+            text.text += ConeCollisions[x].name + '\n';
+        }
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        text.text = "IN";
-        text.color = Color.red;
-        text.text = other.gameObject.name;//.GetComponent<Collider>().transform.parent.ToString();
+        ConeCollisions.Add(other.gameObject);
     }
 
     void OnTriggerExit(Collider other)
     {
         text.text = "OUT";
-        text.color = Color.blue;
+        ConeCollisions.Remove(other.gameObject);
+
     }
 }
