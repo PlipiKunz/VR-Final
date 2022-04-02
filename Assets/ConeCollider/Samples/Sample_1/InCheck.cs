3e02103ad690339bcced03b07f6a89ae2dfbb802
Scott <johnson.scott.14@gmail.com>
2022-04-02 17:51:26 -0600

    added Gather/Scatter to cone script

--- Assets/ConeCollider/Samples/Sample_1/InCheck.cs
+++ Assets/ConeCollider/Samples/Sample_1/InCheck.cs
@@ -7,6 +7,10 @@ public class InCheck : MonoBehaviour {
 
     private Text text;
     public List<GameObject> ConeCollisions;
+    public GameObject Marker;
+    public Vector3 sumPositions;
+    public Vector3 averagePos = new Vector3(-100,-100, - 100);
+    [Range(-1.0f, 1.0f)] public float ScatterGather = 1;
 
     void Awake()
     {
@@ -21,21 +25,48 @@ public class InCheck : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        averagePos = Vector3.zero;
         text.text = "";
+        sumPositions = Vector3.zero;
         for(int x = 0; x < ConeCollisions.Count; x ++){
             text.text += ConeCollisions[x].name + '\n';
         }
+
+        for(int x = 0; x < ConeCollisions.Count; x ++){
+            if (ConeCollisions[x].name != "XR Origin") sumPositions += ConeCollisions[x].transform.position;
+        }
+        averagePos = sumPositions / (ConeCollisions.Count - 1);
+        // Marker.transform.position = averagePos;
+        for(int x = 0; x < ConeCollisions.Count; x ++){
+            if (ConeCollisions[x].name != "XR Origin") 
+            {
+                Vector3 Temp = averagePos - ConeCollisions[x].transform.position;
+                Temp = Temp * ScatterGather * Time.deltaTime;
+                ConeCollisions[x].transform.position += Temp;
+            }
+        }
+        
+
 	}
 
     void OnTriggerEnter(Collider other)
     {
-        ConeCollisions.Add(other.gameObject);
+        if(other.gameObject.tag == "Selectable") {
+            ConeCollisions.Add(other.gameObject);
+            if(other.gameObject.TryGetComponent(out Rigidbody temp)){
+                temp.useGravity = false;
+                temp.velocity = Vector3.zero;
+            }
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         text.text = "OUT";
         ConeCollisions.Remove(other.gameObject);
+        if(other.gameObject.TryGetComponent(out Rigidbody temp)){
+            temp.useGravity = true;
+        }
 
     }
 }
