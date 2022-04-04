e6ca5835d76d956b94429b01964cf75a7c468602
PlipiKunz <johnathankunz@gmail.com>
2022-04-04 16:17:13 -0600

    Added highlighter, using the onHoverINteractable

---
+++ Assets/Custom Assets/Scripts/OnHoverInteractable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class OnHoverInteractable : XRBaseInteractable
+{
+    public bool selected = true;
+    private Outline outline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color c = gameObject.GetComponent<MeshRenderer>().material.color;
+        c = Color.Lerp(c, Color.black, .8f);
+
+        outline = gameObject.AddComponent<Outline>();
+        outline.enabled = false;
+        outline.OutlineMode = Outline.Mode.OutlineVisible;
+        outline.OutlineColor = c;
+        outline.OutlineWidth = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        outline.enabled = selected;
+    }
+
+
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        selected = !selected;
+    }
+
+}
